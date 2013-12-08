13bcb41cbbe75a3a92db684ba524771bc31691b8
Jon Hinks <projects.ws@jondh.me.uk>
2013-12-08 12:44:24 +0000

    Made more progress on account scrape

--- accounts/uk/orange.js
+++ accounts/uk/orange.js
@@ -30,15 +30,125 @@ function containsWarning(haystack, needle)
 
 function login(params)
 {
+	/**
+	 * This function is a class to hold the load handler methods
+	 */
+	function LoadHandler()
+	{
+		this.PAGE_LOGIN = 'login';
+		this.PAGE_EMAIL_REQUEST = 'email';
+		//this.PAGE_ACCOUNT = 'account';
+
+//		function webTestForEmailRequestPage()
+//		{
+//			var title = document.querySelector('.login-flow h2');
+//			console('Check title: ' + title.innerText);
+//		}
+
+		/**
+		 * Handles the loading of the login screen
+		 * 
+		 * @param page
+		 */
+		this.onLoadLogin = function(page)
+		{
+			var title = page.evaluate(
+				function(params)
+				{
+					var element = document.querySelector('h1.PageTitleSignInEE');
+					var title = element ? element.innerText : '';
+
+					// WIP
+					/*
+					if (title.indexOf('SERVICES ON ORANGE.CO.UK') > -1)
+					{
+						// Return login status
+					}
+					*/
+
+					return title;
+				},
+				page.watcherParams
+			);
+
+			// @todo If our cookie means we are already logged in, skip this bit
+			console.log('Contents: ' + page.contents);
+			phantom.exit();
+
+			containsWarning(title, 'SERVICES ON ORANGE.CO.UK');
+
+			page.watcherId = 'LoginSubmitted';
+			page.evaluate(
+				function(params)
+				{
+					// Set up the credentials and submit the form
+					document.querySelector('input[name=LOGIN]').value = params.username;
+					document.querySelector('input[name=PASSWORD]').value = params.password;
+					document.querySelector('div.inner_left_ee form').submit();
+				},
+				page.watcherParams
+			);
+		};
+
+		/**
+		 * Handles the screen after the creds have been submitted
+		 * 
+		 * If the credentials are wrong this is sometimes called twice
+		 * 
+		 * @param page
+		 */
+		this.onLoadLoginSubmitted = function(page)
+		{
+			page.evaluate(
+				function()
+				{
+					// Test for wrong credentials
+					var error = document.querySelector('div.inner_left_ee p.error');
+					if (error)
+					{
+						if (error.indexOf('Please enter a valid username and password') !== -1)
+						{
+							console.log(
+								'[error] The username and password settings are incorrect'
+							);
+							phantom.exit();
+						}
+					}
+				}
+			);
+
+			// Test for preferred email address screen, skip if found
+			console.log(
+				page.contents
+			);
+
+		};
+	}
+
 	// Logon to the system
-	var page = require('webpage').create();
+	var
+		page = require('webpage').create(),
+		loadHandler = new LoadHandler()
+	;
 
 	page.onLoadStarted = function() {
 		console.log('[ok] Page load started: ' + page.watcherId);
 	};
 
+	/**
+	 * This is called, I think, when a redirect happens
+	 * 
+	 * @param targetUrl
+	 */
+	page.onUrlChanged = function(targetUrl) {
+		console.log('[debug] New URL: ' + targetUrl + ' on page: ' + page.watcherId);
+	};
+
 	page.onLoadFinished = function() {
 		console.log('[ok] Page load finished:' + page.watcherId);
+
+		// Call the custom handler, passing the page object in
+		loadHandler['onLoad' + page.watcherId](page);
 	};
 
 	// Thanks to https://www.princeton.edu/~crmarsh/phantomjs/
@@ -48,7 +158,7 @@ function login(params)
 	};
 
 	var url = 'https://web.orange.co.uk/r/login/';
-	page.watcherId = 'login';
+	page.watcherId = 'Login';
 	page.watcherParams = params;
 	page.open(
 		url,
@@ -60,33 +170,6 @@ function login(params)
 			}
 			else
 			{
-				console.log('[debug] Prior to checking initial title');
-				var title = page.evaluate(
-					function()
-					{
-						console.log('[debug] Check initial title running');
-						var element = document.querySelector('h1.PageTitleSignInEE');
-
-						return element ? element.innerText : '';
-					}
-				);
-				containsWarning(title, 'SERVICES ON ORANGE.CO.UK');
-
-				console.log('[debug] Submitting login details');
-				page.watcherId = 'logged-in';
-				page.evaluate(
-					function(params)
-					{
-						console.log('[debug] Submitting login details running');
-
-						// Set up the credentials and submit the form
-//						document.querySelector('input[name=LOGIN]').value = params.username;
-//						document.querySelector('input[name=PASSWORD]').value = params.password;
-						document.querySelector('div.inner_left_ee form').submit();
-					},
-					page.watcherParams
-				);
-
 				// We need to run this on the +next+ page load
 //				page.evaluate(
 //					function()
